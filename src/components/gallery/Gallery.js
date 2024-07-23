11dd023d771f8302fb2faa5900dbf51fb389b5a6
juansanchezilab <judisarodev@gmail.com>
2024-07-23 05:54:59 -0500

    cors

--- src/components/gallery/Gallery.js
+++ src/components/gallery/Gallery.js
@@ -22,8 +22,7 @@ function Gallery({ title, text, typeId }) {
             method: 'GET',
             headers: {
                 'Content-Type': 'application/json'
-            },
-            referrerPolicy: 'strict-origin-when-cross-origin'
+            }
         })
         .then((response) => response.json())
         .then((data) => {
