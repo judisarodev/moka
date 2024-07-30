287578e4652a3d3ce73873490b6ee55085776363
juansanchezilab <judisarodev@gmail.com>
2024-07-30 12:02:14 -0500

    Ajustes de banner home

--- src/components/whatsapp-button/WhatsappButton.js
+++ src/components/whatsapp-button/WhatsappButton.js
@@ -6,7 +6,7 @@ import { FaWhatsapp } from "react-icons/fa";
 function WhatsappButton({ bntText, link , margin = true, small = true}){
     const shortLink = 'https://wa.me/573027343613?text=Hola, Moka, estoy interesado en una torta personalizada www.mokatortas.com';
     const sytles = {
-        fontSize: small ? '0.9rem' : '1.2rem',
+        fontSize: small ? '0.9rem' : '1.1rem',
         fontWeight: small ? '200' : '600',
         margin: margin ? '10px 10% 10px 10%' : '0px',
     }
@@ -15,7 +15,7 @@ function WhatsappButton({ bntText, link , margin = true, small = true}){
             <button onClick={() => { window.open(link ? link : shortLink) }} className="whatsapp-button" style={sytles}>
                 
                     <div className="d-flex align-items-center gap-1">
-                        <FaWhatsapp size={small ? 25 : 35}/>
+                        <FaWhatsapp size={small ? 25 : 28}/>
                         { bntText ? bntText : 'Hablemos por Whatsapp' }
                     </div>
                 
